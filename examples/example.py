b6bb636415b2a59c5b26087f2db332b9926d7dc5
Sergiusz <sergiuszbleja@gmail.com>
2018-06-06 15:28:56 -0400

    Adding examples

---
+++ examples/example.py
@@ -0,0 +1,10 @@
+from duckgoose.image_classification_bootstrap import fetchImagesAndPrepForClassification
+
+# dictionary structure `class_name => search term`
+image_classes = { 'ducks' : 'ducks -rubber' , 'geese' : 'geese' }
+download_path = '/home/myuser/data/downloaded_from_google'
+output_path = '/home/myuser/data/ducksgeese/'
+number_of_images = 100
+
+fetchImagesAndPrepForClassification(image_classes, download_path, output_path, number_of_images)
+
