bcbdca7ac37c165aa319245a464e848e2721fac0
Sergiusz Bleja <sergiusz@bleja.org>
2019-05-25 22:33:02 +0100

    Change version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(name='duckgoose',
-      version='0.1.7',
+      version='0.1.8',
       description='Utility functions for the fast ai mooc',
       url='http://github.com/svenski/duckgoose',
       author='Sergiusz Bleja',
@@ -14,7 +14,7 @@ setup(name='duckgoose',
       packages=['duckgoose'],
       install_requires=['google-images-download'],
       keywords=['fastai','image-classification', 'deep-learning', 'class-activation-maps'],
-      download_url='https://github.com/svenski/duckgoose/archive/0.1.6.tar.gz')
+      download_url='https://github.com/svenski/duckgoose/archive/0.1.8.tar.gz')
 
 
       
